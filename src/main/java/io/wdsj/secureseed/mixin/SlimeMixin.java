cca1629629709ef23bfb6a2af143cbd2e5660917
Sebastian Rasor <https://www.sebastianrasor.com/contact>
2026-07-17 23:12:22 -0500

    Update to 26.2

--- src/main/java/io/wdsj/secureseed/mixin/SlimeMixin.java
+++ src/main/java/io/wdsj/secureseed/mixin/SlimeMixin.java
@@ -5,7 +5,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.monster.Slime;
+import net.minecraft.world.entity.monster.cubemob.Slime;
 import net.minecraft.world.level.LevelAccessor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
